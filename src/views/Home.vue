d20e921ae0abfa993c50140f033b4e4df18068cc
Lauryn <laurynpearson@gmail.com>
2019-07-30 20:47:46 -0500

    add create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>Name: <input type="text" v-model="newProductName"></p>
+    <p>Description: <input type="text" v-model="newProductDescription"></p>
+   <button v-on:click="makeProduct()">Make a new product</button>
     <div v-for="product in products">
       <p>title:{{product.name}}</p>
-      <p>image url: {{product.image_url}}</p>
-      <img v-bind:src="product.image_url" v-bind:alt="product.name"/>
+    <!--   <p>image url: {{product.images_url}}</p> -->
+      <img v-bind:src="product.images_url" v-bind:alt="product.name"/>
       <hr>
     </div>
   </div>
@@ -20,7 +23,9 @@ export default {
   data: function() {
     return {
       message: "Hi!",
-      products: []
+      products: [],
+      newProductName: "",
+      newProductDescription: ""
     };
   },
   created: function() {
@@ -30,6 +35,21 @@ export default {
       this.products = response.data;
     });
   },
-  methods: {}
+  methods: {
+    makeProduct: function() {
+      console.log('make product method');
+      //get some data
+      var newProduct = {
+        name: this.newProductName,
+        Description: this.newProductDescription
+      };
+      //send data to the API
+      axios.post('/api/products', newProduct).then(response => {
+        console.log('in callback for create');
+        console.log(response.data);
+        this.products.push(response.data);
+      });
+    }
+  }
 };
 </script>
