e969ffeaeb742ae36d5cae4eccfee1f6fbdb4ea2
Lauryn <laurynpearson@gmail.com>
2019-07-30 19:50:35 -0500

    create index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <p>title:{{product.name}}</p>
+      <p>image url: {{product.image_url}}</p>
+      <img v-bind:src="product.image_url" v-bind:alt="product.name"/>
+      <hr>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Hi!",
+      products: []
+    };
+  },
+  created: function() {
+    console.log('hi');
+    axios.get("/api/products").then(response => {
+      console.log(response.data);
+      this.products = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
